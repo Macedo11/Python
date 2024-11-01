c2a3c37444ba57910da438a66304514280b60dbb
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-01 10:45:16 -0300

    Criando e finaliando ex33

--- mundo1/ex033.py
+++ mundo1/ex033.py
@@ -1,1 +1,11 @@
 # 33) Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+num3 = int(input("Digite o último número: "))
+
+lista = [num1, num2, num3]
+menor = min(lista)
+maior = max(lista)
+
+print(f"O menor número digitado é: {menor}")
+print(f"O maior número digitado é: {maior}")
