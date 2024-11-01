3d9cfaaf74b824f63505d92bec52efcdd485c3e9
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-01 10:39:14 -0300

    Criando e finalizando ex32

--- mundo1/ex032.py
+++ mundo1/ex032.py
@@ -1,1 +1,6 @@
 # 32) Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+ano = int(input("Digite um ano: "))
+
+if ano % 4 == 00:
+    print(f"{ano} É um ano Bissexto!")
+else: print(f"{ano} NÃO é uma ano Bissexto!")
