dbafda56339b8f18699e7b5d622545ae1178ddfe
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-01 10:21:33 -0300

    Criando e finalizando ex29

--- mundo1/ex029.py
+++ mundo1/ex029.py
@@ -1,2 +1,8 @@
 # 29) Escreva um programa que leia a velocidade do carro. Se ele ultrapassar 80km/h. Mostre uma mensagem dizendo que 
 # ele foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+velo = int(input("Digite a velocidade do carro: "))
+
+if velo >= 80:
+    multa = float((velo - 80) * 7)
+    print(f"Você ultrapassou o limite de velocidade de 80Km/h!")
+    print(f"Multado no valor de R${multa:.2f}")
