06f3cb8c926fbd4dd78d510bfbf0d4261afdb91e
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-01 10:30:11 -0300

    Criando e finaliando ex31

--- mundo1/ex031.py
+++ mundo1/ex031.py
@@ -1,1 +1,9 @@
 # 31) Desenvolva um programa que pergunte a distância de uma viagem em Km. Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200km e R$0,45 para viagens mais longas.
+distancia = int(input("Digite a distância da viagem: "))
+
+if distancia <= 200:
+    valor = float(distancia * 0.50)
+    print(f"Sua viagem custará: R${valor:.2f}")
+else:
+    valor = float(distancia * 0.45)
+    print(f"Sua viagem custará: R${valor:.2f}")
