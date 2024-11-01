6809cf9e22af8ae34c4f57b1dc6a6b6d4aaa9531
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-01 10:57:30 -0300

    Criando e finalizando ex34

--- mundo1/ex034.py
+++ mundo1/ex034.py
@@ -1,1 +1,9 @@
 # 34) Escreva um programa que pergunte o salário e calcule o valor do seu aumento. Para salários superiores a R$1.250,00, calcule um aumento de %10. Para os inferiores ou iguais o aumento é de 15%.
+salario = float(input("Digite o valor do seu salário: "))
+
+if salario > 1250:
+    novo_salario = salario + (salario * 0.1)
+    print(f"Você ganhou um aumento de 10%: R${novo_salario:.2f}")
+else:
+    novo_salario = salario + (salario * 0.15)
+    print(f"Você ganhou um aumento de 15%: R${novo_salario:.2f}")
