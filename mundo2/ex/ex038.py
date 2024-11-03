d6fad889222ce4f2aa529db53bb178b559485e28
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-03 13:12:25 -0300

    Criando e finalizando ex38

--- mundo2/ex/ex038.py
+++ mundo2/ex/ex038.py
@@ -1,2 +1,11 @@
 # 38) Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
-# -O primeiro valor é maior; -O segundo valir é maior; -Não existe valor maior, os dois são iguais.
+# -O primeiro valor é maior; -O segundo valor é maior; -Não existe valor maior, os dois são iguais.
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if num1 > num2:
+    print(f"O primeiro valor é maior")
+elif num2 > num1: 
+    print(f"O segundo valor é maior")
+else:
+    print("Não existe valor maior, os dois são iguais")
