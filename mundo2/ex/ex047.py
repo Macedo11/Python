3b4bce2c9f075973c9fed90badf9daa9ea8e8e58
João Victor da Silva Macedo <123326038+Macedo11@users.noreply.github.com>
2024-11-12 22:32:36 -0300

    Criand e finalizando ex47

--- mundo2/ex/ex047.py
+++ mundo2/ex/ex047.py
@@ -1,4 +1,7 @@
 # 47) Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
-for i in range (1, 50):
-    print
+for i in range (0, 51, 2):
+    if i % 2 == 0:
+        print(i, end=" ")
+    else:
+        print("Não existe números pares")
